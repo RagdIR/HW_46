84ba0247cd5d7e03c20ba2060e380b71c6b018fe
RagdIR <ragdirstrogov@gmail.com>
2020-07-21 20:18:06 +0600

    Добавлены страницы index и add, настроены вьюшки

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view, note_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view),
+    path('notes/add/', note_create_view),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, 'index.html')
+
+def note_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'note_create.html')
+    elif request.method == 'POST':
+        print(request.POST)
