d661fcf8cba45c2f04cd4dfffb91d9c25fd1be26
RagdIR <ragdirstrogov@gmail.com>
2020-07-23 17:29:10 +0600

    Отредактированы вьюшки, сделана запись в бд.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from webapp.models import Note
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    data = Note.objects.all()
+    return render(request, 'index.html', context={'notes': data})
 
 def note_create_view(request):
     if request.method == 'GET':
         return render(request, 'note_create.html')
     elif request.method == 'POST':
-        print(request.POST)
+        title = request.POST.get('title')
+        text = request.POST.get('content')
+        note = Note.objects.create(title=title, text=text)
+        context = {'note': note}
+        return render(request, 'note_view.html', context)
+
