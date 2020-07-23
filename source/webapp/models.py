02d01f5ad5a0d00f3359cf26b196c538c5e3ebf9
RagdIR <ragdirstrogov@gmail.com>
2020-07-23 15:18:15 +0600

    Исправлена модель под записи, исправлена ссылка на базу данных, добален суперюзер и style.css

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 
 class Note(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Название')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
-    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    status = models.CharField(max_length=20, default='new', verbose_name="Не проверено")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.title)
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
